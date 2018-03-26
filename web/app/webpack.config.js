52d75d740903ccba871b6963df414cef5f452017
jaychia <jaychia94@gmail.com>
2018-03-26 19:54:44 -0400

    deleted node_modules

--- web/app/webpack.config.js
+++ web/app/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: __dirname + "/js/main.js",
+    entry: __dirname + "/static/js/main.js",
     output: {
         path: __dirname + '/static',
         filename: "bundle.js"
