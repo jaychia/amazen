48665b36aedbdaac6cf5a0d4485af7fb34b57bc5
charlesxbai <chxbai@hotmail.com>
2018-04-15 01:33:03 -0400

    finish state

--- web/app/static/js/bundled/main.js
+++ web/app/static/js/bundled/main.js
@@ -19168,6 +19168,8 @@ var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -19183,14 +19185,22 @@ var Search = function (_React$Component) {
     var _this = _possibleConstructorReturn(this, (Search.__proto__ || Object.getPrototypeOf(Search)).apply(this, arguments));
 
     _this.state = { descriptors: [] };
+    _this.buttonOnClick = _this.buttonOnClick.bind(_this);
     return _this;
   }
-  //
-  // buttonOnClick = () => {
-  //   this.setState({descriptors = this.state.descriptors.add(this.refs.New_descriptor.value)});
-  // };
 
   _createClass(Search, [{
+    key: "buttonOnClick",
+    value: function buttonOnClick() {
+      var _this2 = this;
+
+      if (this.refs.New_descriptor.value != "") this.setState(function (prevState, props) {
+        return {
+          descriptors: [].concat(_toConsumableArray(prevState.descriptors), [_this2.refs.New_descriptor.value])
+        };
+      });
+    }
+  }, {
     key: "render",
     value: function render() {
       return _react2.default.createElement(
@@ -19232,11 +19242,22 @@ var Search = function (_React$Component) {
               { className: "input-group-btn" },
               _react2.default.createElement(
                 "button",
-                { className: "btn btn-lg", type: "button" },
+                { className: "btn btn-lg", type: "button", onClick: this.buttonOnClick },
                 _react2.default.createElement("span", { className: "glyphicon glyphicon-plus" })
               )
             )
           )
+        ),
+        _react2.default.createElement(
+          "ul",
+          null,
+          this.state.descriptors.map(function (message) {
+            return _react2.default.createElement(
+              "li",
+              { key: message },
+              message
+            );
+          })
         )
       );
     }
