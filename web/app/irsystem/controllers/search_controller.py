29544476ce5cba7b7dc8454f54d568f1f40dc561
jaychia <jaychia94@gmail.com>
2018-04-14 22:38:42 -0400

    changed docker compose to allow for persistent database

--- web/app/irsystem/controllers/search_controller.py
+++ web/app/irsystem/controllers/search_controller.py
@@ -1,9 +1,37 @@
 from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
+from app.irsystem.models.product import products_with_pids
+from flask import jsonify
 
 project_name = "Amazen"
 net_id = "Joo Ho Yeo (jy396) | Amritansh Kwatra (ak2244) | Alex Yoo (ay244) | Jay Chia (jc2375) | Charles Bai (cb674)"
+
+###################################################################################################
+# Placeholder functions
+##################################################################################################
+def classify_query(q):
+	return "electronics"
+
+def filter_category_by_query(q, cat):
+	return ["1234", "123", "12"]
+
+def rank_pids_with_desc(descs, pid):
+	return ["1234", "123", "12"]
+
+def pack_pid_json(pids):
+	products = products_with_pids(pids)
+	return [{'product_id': p.azn_product_id,
+			'ratings': 5.0, #TODO: Update
+			'keywords': ['Hello', 'Goodbye'], #TODO: Update
+			'keywordscores': [4.0, 2.0], #TODO: Update
+			'price': p.price,
+			'name': p.name,
+			'seller_name': p.seller_name} for p in products]
+
+##################################################################################################
+# Views
+##################################################################################################
 
 @irsystem.route('/', methods=['GET'])
 def search():
@@ -15,3 +43,32 @@ def search():
 		output_message = "Your search: " + query
 		data = range(5)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+
+@irsystem.route('search_page', methods=['GET'])
+def search_page():
+	initial_query = request.args.get('query')
+	initial_descriptors = request.args.get('descriptors', [])
+	if initial_query is None:
+		return render_template('search.html')
+	return render_template('search_page.html', query=initial_query, descriptors=initial_descriptors)
+
+@irsystem.route('search', methods=['GET'])
+def product_search():
+	query = request.args.get('query')
+	descriptors = request.args.get('descriptors', [])
+	if not query:
+		d = {
+			'status': 400,
+			'error_message': 'empty query provided'
+		}
+		return jsonify(d)
+	
+	category = classify_query(query.strip().lower())
+	pids = filter_category_by_query(query, category)
+	if len(descriptors) > 0:
+		descriptors = [x.lower().strip() for x in descriptors]
+		sorted_pids = rank_pids_with_desc(descriptors, pids)
+	else:
+		sorted_pids = pids
+	d = pack_pid_json(sorted_pids)
+	return jsonify(d)
--- web/app/irsystem/models/product.py
+++ web/app/irsystem/models/product.py
@@ -6,7 +6,7 @@ class Product(Base):
   name = db.Column(db.String(), nullable=False)
   price = db.Column(db.Float(), nullable=False)
   img_url = db.Column(db.String(), nullable=False)
-  azn_product_id = db.Column(db.String(), nullable=False)
+  azn_product_id = db.Column(db.String(), unique=True, nullable=False, index=True)
   seller_name = db.Column(db.String(), nullable=False)
   #average_stars = db.Column(db.Float(), nullable=False)
   #keywords = db.Column(db.String(), nullable=False)
@@ -19,9 +19,9 @@ class Product(Base):
       Float [price]
       String [img_url]
     """
-    self.name     = name
-    self.price    = price
-    self.img_url  = img_url
+    self.name = name
+    self.price = price
+    self.img_url = img_url
     self.azn_product_id = azn_product_id
     self.seller_name = seller_name
     #self.average_stars = average_stars
@@ -30,6 +30,9 @@ class Product(Base):
   def __repr__(self):
     return str(self.__dict__)
 
+def products_with_pids(pid_list):
+  products = Product.query.filter(Product.azn_product_id.in_(pid_list)).all()
+  return [next(s for s in products if s.azn_product_id == id) for id in pid_list]
 
 class ProductSchema(ModelSchema):
   class Meta:
