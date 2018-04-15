c4f53c6feec5bd66b4b8f670b2197aa6d54b82f7
jaychia <jaychia94@gmail.com>
2018-04-15 00:44:48 -0400

    REQUIRES docker-compose build again - fixed gulpfile to add new main js

---
+++ web/app/static/js/main_search_page.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Search from './Search.jsx';
+
+ReactDOM.render(<Search name={name} netid={netid} />, document.getElementById('root'));
