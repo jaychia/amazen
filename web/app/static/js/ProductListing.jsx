26ace396a4dd6ef02ca89d24575f3a553c5ff49b
charlesxbai <chxbai@hotmail.com>
2018-04-23 17:58:48 -0400

    pop up

--- web/app/static/js/ProductListing.jsx
+++ web/app/static/js/ProductListing.jsx
@@ -1,11 +1,97 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Popup from 'react-popup';
 
 const keyword_colors = [[251, 147, 44], [203, 152, 48], [164, 157, 52], [133, 162, 57], [107, 167, 63], [87, 172, 68], [70, 177, 75], [57, 183, 81], [46, 189, 89]];
 
 export default class ProductListing extends React.Component {
   constructor() {
     super(...arguments);
+    this.keywordOnClick = this.keywordOnClick.bind(this);
+  }
+
+  addTermPlot(term, rating_freq) {
+    // SVG params
+    var h = 150;
+    var w = 600;
+    var m = 20
+    var svg = d3.select(document.createElementNS('http://www.w3.org/2000/svg', 'svg'))
+      .attr("height", h + m)
+      .attr("width", w + m);
+    var text = svg.append("text")
+      .text(term)
+      .attr("x", 10)
+      .attr("y", 25)
+      .style("font-size", 18);
+    // define scales and axis
+    var xScale = d3.scaleOrdinal()
+      .domain(["1", "2", "3", "4", "5"])
+      .range([0 + m, w / 5 + m, 2 * w / 5 + m, 3 * w / 5 + m, 4 * w / 5 + m]);
+    var yScale = d3.scaleLog()
+      .domain([Math.exp(-10), 1000])
+      .range([h - m / 2, 0]);
+    var xAxis = d3.axisBottom(xScale);
+    svg.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + (h) + ")")
+      .call(xAxis);
+    // add points from user input in rating_freq
+    var points = []
+    Object.keys(rating_freq).forEach(function (key) {
+      points.push({ x: xScale(Number(key)), y: yScale(rating_freq[key] + Math.exp(-10)) });
+    });
+    // define line and area generators
+    var lineGenerator = d3.line()
+      .x(function (d) { return d.x; })
+      .y(function (d) { return d.y; })
+      .curve(d3.curveCardinal);
+    var area = d3.area()
+      .x(function (d) { return d.x; })
+      .y0(h)
+      .y1(function (d) { return d.y; })
+      .curve(d3.curveCardinal);
+    // define fill/stroke gradient target
+    var grad = svg.append("linearGradient")
+      .attr("id", "linGrad");
+    var stop1 = grad.append("stop")
+      .attr("id", "stop1")
+      .attr("offset", "0%")
+      .attr("stop-color", "#f00000")
+      .attr("stop-opacity", "0.9");
+    var stop2 = grad.append("stop")
+      .attr("id", "stop2")
+      .attr("offset", "100%")
+      .attr("stop-color", "#00aa3f");
+    // append paths and area
+    var pathData = lineGenerator(points);
+    svg.append("path")
+      .data([points])
+      .attr("class", "area")
+      .attr("d", area)
+      .style("fill", "url(#linGrad)")
+      .style("opacity", "0.3");
+    svg.append('path')
+      .attr('d', pathData)
+      .attr("class", "path")
+      .style("stroke", "url(#linGrad)")
+      .style("stroke-width", 4)
+      .style("fill", "None");
+    var doctype = '<?xml version="1.0" standalone="no"?>'
+      + '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
+    var source = (new XMLSerializer()).serializeToString(svg.node());
+    var blob = new Blob([doctype + source], { type: 'image/svg+xml;charset=utf-8' });
+    var url = window.URL.createObjectURL(blob);
+    var img = <img src={url} />;
+    return img;
+  } 
+
+  keywordOnClick(word, score_list) {
+    var img = this.addTermPlot(word, score_list);
+    Popup.create({
+      title: 'Keyword Detail',
+      content: img,
+      className: 'alert',
+    }, true);
   }
 
   render() {
@@ -26,18 +112,19 @@ export default class ProductListing extends React.Component {
     if (rounded % 1 != 0) { stars.push(<img src="/static/img/halfstar.png" className="half-star" />);}
     stars.push(<span key={this.props.numRatings} className="num-ratings">{this.props.numRatings}</span>)
 
-    let keywords = this.props.keywords.map((e, i) => [e, this.props.keywordscores[i]]);
+    let keywords = this.props.keywords.map((e, i) => [e, this.props.keywordscores[i], this.props.keywordScoreList[i]]);
     let div = 5.0 / 8;
     let k2_to_div = (k2) => {
       let rgb = keyword_colors[Math.floor(k2[1] / div)];
       let rgb_str = 'rgb(' + rgb[0].toString() + "," + rgb[1].toString() + "," + rgb[2].toString() + ", 1" + ')';
       let colorStyle = {'backgroundColor': rgb_str};
-      return <div className="keyword" style={colorStyle}>{k2[0]}</div>
+      return <button className="keyword" style={colorStyle} type="button" onClick={() => this.keywordOnClick(k2[0], k2[2])}>{k2[0]}</button>
     }
     let keyword_divs = keywords.map(k2_to_div);
 
     return (
       <div className="product-listing-container">
+        <Popup />
         <div className="product-listing">
           <div className="product-img-container">
             <img className="product-img" src={this.props.imgUrl}/>
