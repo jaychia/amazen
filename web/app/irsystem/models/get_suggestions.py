43e8058f4b3916206f65ef5243475a231dfbcda3
Amrit Kwatra <Amrit@dhcp-rhodes-251.redrover.cornell.edu>
2018-04-29 03:18:22 -0400

    added the redis to app

--- web/app/irsystem/models/get_suggestions.py
+++ web/app/irsystem/models/get_suggestions.py
@@ -1,15 +1,16 @@
-import redis 
+from app import rdb_01
 import ast 
 from collections import Counter
 
-r = redis.StrictRedis(host='localhost', port=6379, db=1)
-N = 5
+N = 10
 
 def get_suggestions(query):
 	split_query = query.split(" ")
 	all_suggestions = Counter()
-	for term in split_query:
-		term_suggestions = ast.literal_eval(r.get(term))
-		all_suggestions+= Counter(dict(term_suggestions))
-
-	return all_suggestions.most_common(N)
+	try:
+		for term in split_query:
+			term_suggestions = ast.literal_eval(rdb_01.get(term))
+			all_suggestions+= Counter(dict(term_suggestions))
+		return map(lambda x: x[0], all_suggestions.most_common(N))
+	except:
+		return []
