ced4f3bf27d27a5824bf6cef97c8bc131dffc28a
Amrit Kwatra <Amrit@dhcp-rhodes-251.redrover.cornell.edu>
2018-04-29 23:06:17 -0400

    stopped pruning bad suggestions

--- redis-app/load_suggestions.py
+++ redis-app/load_suggestions.py
@@ -13,5 +13,4 @@ with open("suggest_title_jTokens") as infile:
 		count += 1
 		v_list = sorted(dd[k].items(),key=lambda x: x[1], reverse = True)
 		#prune all words that cooccur less than 5 times
-		v_list = filter(lambda x : x[1]>=5, v_list)
 		r.set(k,v_list)
