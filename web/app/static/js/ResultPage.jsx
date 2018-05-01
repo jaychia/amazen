2a26b9dcadedfff34893be9591d7c138daa1291c
charlesxbai <chxbai@hotmail.com>
2018-04-30 22:42:34 -0400

    add load icon for result page

--- web/app/static/js/ResultPage.jsx
+++ web/app/static/js/ResultPage.jsx
@@ -14,8 +14,10 @@ export default class ResultPage extends React.Component {
     }
 
     componentDidMount() {
+        this.setState({ loading: true });
         axios.get("/search?query=" + this.props.query + "&positive=" + this.props.positive + "&negative=" + this.props.negative)
             .then(res => {
+                this.setState({ loading: false });
                 if (res.data.data.length > 0) {
                     this.setState({ products: res.data.data, suggestions: "" });
                 } else {
@@ -25,7 +27,11 @@ export default class ResultPage extends React.Component {
     }
 
     render() {
-        let body = (this.state.suggestions.length == 0) ?
+        let body = (this.state.loading) ?
+        (<div className="loading-icon-wrapper">
+            <img src="/static/img/loading.svg" alt="loading" style={{ width: '100px', marginTop : '10%' }} />
+        </div>) :
+        ((this.state.suggestions.length == 0) ?
             this.state.products.map(function (p, i) {
                 return <ProductListing
                     key={i}
@@ -43,7 +49,10 @@ export default class ResultPage extends React.Component {
                     imgUrl={p.imgUrl}
                     numRatings={p.numRatings}
                     asin={p.asin} />
-            }) : <div className="error-container"> <img src="static/img/404.png" className="four-oh-four-img"></img><span>Did you mean to type: {this.state.suggestions}</span></div>;
+            }) : (<div className="error-container"> 
+                    <img src="static/img/404.png" className="four-oh-four-img" />
+                    <span>Did you mean to type: {this.state.suggestions}</span>
+                </div>));
         return (
             <div>
                 <div className="result-page-bar-background">
